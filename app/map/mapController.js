7f200b3d5543a47f33367251d103fb6527abb05d
roilabs <w@pyszko.net>
2016-10-18 15:48:29 +0100

    factory

--- app/map/mapController.js
+++ app/map/mapController.js
@@ -3,90 +3,94 @@
     var app = angular.module('mapApp');
     app.controller('mapController', mapController);
 
-    function mapController($scope, $http) {
+    function mapController($scope, $http, mapFactory) {
 
         vm = this
 
-        var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+        mapFactory.getData().then(display)
 
+        function display(response) {
+            vm.dclocations = response.data;
+        }
+
+        var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+        var targetZoomLevel = 25;
+        var targetScale = 0.5;
         vm.testdata = [{
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Basingstoke",
             latitude: 51.2665,
             longitude: -1.0924,
-            "description": "OTDC",
+            "label": "OTDC"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Farnborough",
             latitude: 51.2869,
             longitude: -0.7526,
-            "description": "OTDC2",
+            "label": "OTDC2"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "London",
             latitude: 51.5002,
             longitude: -0.1262,
-            "label": "London",
-            "description": "60 Victoria Embancment",
+            "label": "London"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Tokyo",
             latitude: 35.6785,
             longitude: 139.6823,
+            "label": "Tokyo"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Hong Kong",
             latitude: 22.3964,
             longitude: 114.1095,
+            "label": "Hong Kong"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Singapore",
             latitude: 1.2894,
             longitude: 103.8500,
             "label": "Singapore",
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "New York",
             latitude: 40.8404,
             longitude: -74.0907,
-            "label": "New York",
-            "description": " Carlstad DC",
+            "label": "New York"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Delaware",
             latitude: 39.6293,
             longitude: -75.6583,
-            "description": "CDC1",
+            "label": "CDC1"
         }, {
             svgPath: targetSVG,
-            zoomLevel: 5,
-            scale: 0.5,
+            zoomLevel: targetZoomLevel,
+            scale: targetScale,
             title: "Dallas",
             latitude: 32.974586,
             longitude: -96.889900,
-            "description": "EDC1",
+            "label": "EDC1"
         }]
 
-        $http.get('data/locations.json')
-            .then(function (response) {
-                vm.dclocations = response.data;
-            });
+        
 
         var map = AmCharts.makeChart("mapdiv", {
             type: "map",
---
+++ app/map/mapFactory.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    var app = angular.module('mapApp');
+    app.factory('mapFactory', mapFactory);
+
+    function mapFactory($http) {
+
+        return {
+            getData : getDataImp
+        };
+
+        function getDataImp() {
+            return $http.get('data/locations.txt')
+        }
+
+
+    }
+
+})();
